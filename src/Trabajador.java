10b66fea700cd1ff25cfbdf45937733df383b03a
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2023-11-20 08:52:00 +0100

    1

---
+++ src/Trabajador.java
@@ -0,0 +1,17 @@
+public class Trabajador {
+    private String nombre;
+    private Double sueldo;
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+    public void setSueldo(double v) {
+        this.sueldo=sueldo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String pepe){
+        this.nombre=nombre;
+    }
+}
